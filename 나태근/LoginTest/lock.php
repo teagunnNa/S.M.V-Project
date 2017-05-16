386d4c018ac8b80b84c6b22b1d949031d8223f67
나태근 <gunn93@naver.com>
2017-05-16 21:49:11 +0900

    회원가입 및 회의록, Dgm

---
+++ 나태근/LoginTest/lock.php
@@ -0,0 +1,33 @@
+<?php
+
+include('config.php');
+
+session_start();
+
+$user_check=$_SESSION['login_user'];
+
+ 
+
+$ses_sql=mysql_query("select name from member where name='$user_check' ");
+
+ 
+
+$row=mysql_fetch_array($ses_sql);
+
+ 
+
+$login_session=$row['name'];
+
+ 
+
+if(!isset($login_session))
+
+{
+
+header("Location: login.php");
+
+}
+
+ 
+
+?>
