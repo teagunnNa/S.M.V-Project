51fc93f8383eeb100ee2797faccfb29b59d7b00b
이윤배 <yc9870@naver.com>
2017-05-23 21:21:28 +0900

    manager 수정

--- 웹/manager.php
+++ 웹/manager.php
@@ -5,30 +5,18 @@
 	</head>
 	<body>
 		<h2>◆ 예약현황</h2>
-		예약장소
-			<select id='place' name='place'>
-				<option value='1'>본관</option>
-				<option value='2'>인문관</option>
-				<option value='3'>자연관</option>
-				<option value='4'>원화관</option>
-				<option value='5'>도서관</option>
-				<option value='6'>체육관</option>
-				<option value='7'>선학음악당</option>
-				<option value='8'>잔디구장</option>
-				<option value='9'>공대운동장</option>
-				<option value='10'>테니스장</option>
-			</select>
-		<td><input type='submit' value='조회'></td>
 		<?php 
+			$room_id = $POST['room_id'];
 			$con = mysqli_connect("localhost","lee","1234","smr","3306");
-			$result = mysqli_query($con,"select * from reservation");
+			$result = mysqli_query($con,"select * from reservation where room_id is $room_id");
 			$number = 1;
 		?>
 		<table width = "1000" border= "1" cellpadding="20">
 		<tr align="center">
 			<td bgcolor="#cccccc">일련번호</td>
 			<td bgcolor="#cccccc">신청자 ID</td>
-			<td bgcolor="#cccccc">예약시간</td>
+			<td bgcolor="#cccccc">사용일자</td>
+			<td bgcolor="#cccccc">사용시간</td>
 			<td bgcolor="#cccccc">사용목적</td>
 			<td bgcolor="#cccccc">수락여부</td>
 			<td bgcolor="#cccccc">거부</td>
@@ -42,10 +30,11 @@
 					<from action='managerAllowed.php' method='POST'>
 					<td> $number</td>
 					<td> $row[ID]</td>
-					<td> $row[Time]</td>
+					<td> $row[Time]:00~($row[Time]+1):00</td>
+					<td> $row[Date]</td>
 					<td> $row[Usage]</td>
-					<td><input type='button' value='수락' onclick='window.open('managerAllowed.php','win','width=350,height=250)'></td>
-					<td><input type='button' value='거절' onclick='location.href='managerDelete.php''></td>
+					<td><input type='submit' value='수락' href='managerAllowed.php=$row[ID]'></td>
+					<td><input type='button' value='거절' href='managerDelete.php=$row[ID]'></td>
 					</from>
 					</tr>
 					");
@@ -55,7 +44,8 @@
 					<tr>
 					<td> $number</td>
 					<td> $row[ID]</td>
-					<td> $row[Time]</td>
+					<td> $row[Time]:00~($row[Time]+1):00</td>
+					<td> $row[Date]</td>
 					<td> $row[Usage]</td>
 					<td> 수락됨 </td>
 					<td><input type='button' value='거절' onclick='location.href='managerDelete.php''></td>
--- 웹/managerDelete.php
+++ 웹/managerDelete.php
@@ -1,11 +1,9 @@
-<?php 
+<?php
 	$ID = $_POST['ID'];
 	
 	$con = mysqli_connect("localhost","root","apmsetup","smr","3306");
-	$que = "update reservation set Allowed='1' where ID is '$ID'";
+	$que = "dalete from reservation where ID=$ID";
 	$result = mysqli_query($con, $que);
-	
-	echo $ID;
 	mysqli_close($con);
-	echo "수락 승인되었습니다.<br>";
+	echo "거절하였습니다."; 
 ?>
